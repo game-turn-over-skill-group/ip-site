ee4d99f5ce86f3b7be2de33691cb471b5fbb498e
仁二 <17070453@qq.com>
2019-10-31 06:05:58 +0800

    Update ip6.cmd

--- code/ip6.cmd
+++ code/ip6.cmd
@@ -6,7 +6,8 @@ curl ip.zxinc.org/api.php?ip=%ip%
 goto nul
 
 :myipv6
-curl http://v6.ipv6-test.com/api/myip.php
+:curl http://v6.ipv6-test.com/api/myip.php
+curl https://api-ipv6.ip.sb/ip
 
 :nul
 @echo off
