13e0a90af8cc46778925733a5878b4429eb4734d
仁二 <17070453@qq.com>
2024-06-04 04:11:29 +0800

    Update ip.cmd
    使用jq命令过滤 + 替换查询URL

--- code/ip.cmd
+++ code/ip.cmd
@@ -5,11 +5,24 @@
 set ip=%1%
 
 ::查询ip归属地
-curl www.evansfix.com/ip.php?ip=%ip%
+
+rem 获取 JSON 数据并存储到临时文件
+curl -s https://qifu-api.baidubce.com/ip/geo/v1/district?ip=%1 > temp.json
+
+rem 使用 jq 解析 JSON 数据，并提取出 country、prov 和 city 的值
+jq -r ".data | \"IP/From: %ip% \nIP/From: \(.country // \"Unknown\") \(.prov // \"\") \(.city // \"\") \(.isp // \"\")\"" temp.json
+
+rem 删除临时文件
+del temp.json
+
+
+
+
 
 
 
 ::备用 查询ip归属地
+::curl www.evansfix.com/ip.php?ip=%ip%
 ::curl ip.zxinc.org/api.php?type=text&ip=%ip%
 ::curl myip4.ipip.net
 ::curl 4.ipw.cn
