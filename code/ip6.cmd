b4a4749f6a5ea1e20633d9dae0a844fd0053da4b
仁二 <17070453@qq.com>
2025-01-16 10:28:52 +0800

    修复ip2(腾讯)脚本 分离ipv4/ipv6数据库 查询返回结果显示 修复ip6脚本在win10(20H2)中多次执行(cmd备注bug)
    修复ip2(腾讯)脚本 分离ipv4/ipv6数据库 查询返回结果显示
    修复ip6脚本在win10(20H2)中多次执行(cmd备注bug)

--- code/ip6.cmd
+++ code/ip6.cmd
@@ -2,8 +2,9 @@
 
 @echo off
 
+chcp 65001 >nul
 
-set ip=%1%
+set ip=%1
 
 if "%1%"=="" goto MyIP
 
@@ -23,31 +24,31 @@ goto End
 
 
 
-::备用 获取ipv6地址
-::curl http://myip6.ipip.net
-::curl https://6.ipw.cn
-::curl http://api-ipv6.ip.sb/ip
-::curl https://v6.ip.zxinc.org/getip
-::curl https://v6.ipv6-test.com/api/myip.php
-::curl https://test.ipw.cn -6
-::curl https://ip.ddnspod.com -6
-::curl http://ipv6.ddnspod.com
-::curl https://api64.ipify.org/?format=text
-::curl -s https://ipvx.netart.cn/ | grep -oE "([0-9]{1,3}\.){3}[0-9]{1,3}|([0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}"
-::curl -s https://ipv6.netart.cn/ | grep -oE "([0-9]{1,3}\.){3}[0-9]{1,3}|([0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}"
-::curl https://ip.guide/%1%
-::curl https://ip.ddnspod.com/geoip/%1%
-::curl https://qifu-api.baidubce.com/ip/geo/v1/ipv6/district?ip=%1%
-::curl https://ipv6.netart.cn/%ip%
-::curl https://ipvx.netart.cn/%ip%
+:: 备用 获取ipv6地址
+rem curl http://myip6.ipip.net
+rem curl https://6.ipw.cn
+rem curl http://api-ipv6.ip.sb/ip
+rem curl https://v6.ip.zxinc.org/getip
+rem curl https://v6.ipv6-test.com/api/myip.php
+rem curl https://test.ipw.cn -6
+rem curl https://ip.ddnspod.com -6
+rem curl http://ipv6.ddnspod.com
+rem curl https://api64.ipify.org/?format=text
+rem curl -s https://ipvx.netart.cn/ | grep -oE "([0-9]{1,3}\.){3}[0-9]{1,3}|([0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}"
+rem curl -s https://ipv6.netart.cn/ | grep -oE "([0-9]{1,3}\.){3}[0-9]{1,3}|([0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}"
+rem curl https://ip.guide/%1%
+rem curl https://ip.ddnspod.com/geoip/%1%
+rem curl https://qifu-api.baidubce.com/ip/geo/v1/ipv6/district?ip=%1%
+rem curl https://ipv6.netart.cn/%ip%
+rem curl https://ipvx.netart.cn/%ip%
 
 
-::无排版 双行显示 IP+归属地
-::curl -s v6.ip.zxinc.org/api.php?tpye=text^&ip=%1% | grep -oP "(?<=<query>).*?(?=</query>)|(?<=<location>).*?(?=</location>)" | sed "s/&#x9;//g"
-::无排版 双行显示 IP+归属地+速度显示
-::curl v6.ip.zxinc.org/api.php?tpye=text^&ip=%1% | grep -oP "(?<=<query>).*?(?=</query>)|(?<=<location>).*?(?=</location>)" | sed "s/&#x9;//g"
-::排版后 整合显示 IP+归属地
-::curl -s v6.ip.zxinc.org/api.php?tpye=text^&ip=%1% | grep -oP "(?<=<query>).*?(?=</query>)|(?<=<location>).*?(?=</location>)" | sed "s/&#x9;//g" | awk '{print "IP/From："$0" \t "$3" "}'
+:: 无排版 双行显示 IP+归属地
+rem curl -s v6.ip.zxinc.org/api.php?tpye=text^&ip=%1% | grep -oP "(?<=<query>).*?(?=</query>)|(?<=<location>).*?(?=</location>)" | sed "s/&#x9;//g"
+:: 无排版 双行显示 IP+归属地+速度显示
+rem curl v6.ip.zxinc.org/api.php?tpye=text^&ip=%1% | grep -oP "(?<=<query>).*?(?=</query>)|(?<=<location>).*?(?=</location>)" | sed "s/&#x9;//g"
+:: 排版后 整合显示 IP+归属地
+::rem curl -s v6.ip.zxinc.org/api.php?tpye=text^&ip=%1% | grep -oP "(?<=<query>).*?(?=</query>)|(?<=<location>).*?(?=</location>)" | sed "s/&#x9;//g" | awk '{print "IP/From："$0" \t "$3" "}'
 
 
 @echo off
--- code/ip_Tencent(=ip2).cmd
+++ code/ip_Tencent(=ip2).cmd
@@ -2,12 +2,47 @@
 
 @echo off
 
-set ip=%1%
+:: 让控制台使用utf-8编码
+chcp 65001 >nul
+
+rem 输出调试路径
+:: echo 当前路径: %cd%
+:: echo 脚本所在路径: %~dp0
+:: echo 系统环境变量：%PATH%
+
+:: 获取第一个参数作为 IP 地址
+set ip=%1
+
+:: 判断 IP 是否为空
+if "%ip%"=="" (
+    rem 如果 IP 为空，优先使用 IPv6 的 API
+    :: echo The IP address is: null
+    set query_url=https://ip.ddnspod.com/geoip2/
+) else (
+    :: 判断是否包含 : 符号（IPv6 地址）
+    echo %ip% | findstr /C:":" >nul
+    if not errorlevel 1 (
+        :: echo The IPV6 address is: %ip%
+        set query_url=https://ip.ddnspod.com/geoip2/%ip%
+    ) else (
+        :: 判断是否包含 . 符号（IPv4 地址）
+        echo %ip% | findstr /C:"." >nul
+        if not errorlevel 1 (
+            :: echo The IPV4 address is: %ip%
+            set query_url=https://ip.ddnspod.com/geoip/%ip%
+        ) else (
+            :: 如果不是 IPv4 或 IPv6 地址
+            echo Invalid IP address: %ip%
+            exit /b 1
+        )
+    )
+)
 
 ::查询ip归属地
 
 :: 获取 JSON 数据并存储到临时文件
-curl -s https://ip.ddnspod.com/geoip/%1 > temp.json
+::curl -s https://ip.ddnspod.com/geoip/%1 > temp.json
+curl -s %query_url% > temp.json
 
 :: 使用 jq 解析 JSON 数据，并提取出 ipv4、ipv6 和 disp 的值
 jq -r "\"IP/From: \(.ipv4 // .ipv6)\nIP/From: \(.disp // \"Unknown\")\"" temp.json
